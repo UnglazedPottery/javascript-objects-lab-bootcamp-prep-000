187753dd82225dd7eb91303810e500fbb283d05f
Jade Huynh <35jadeh@gmail.com>
2019-07-04 17:24:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {bread: "starter"}
+
+updateObjectWithKeyAndValue(object, key, value)
+{
+  return Object.assign({}, object , {[key]: value})
+}
+
+destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  return Object.assign(object, {[key]: value})
+}
+
+deleteFromObjectByKey(object, key)
+{
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+destructivelyDeleteFromObjectByKey(object, key)
+{
+  delete object[key]
+  return key
+}
